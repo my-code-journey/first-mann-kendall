5e5537029753f48d1cbb39fa7852d2af2c9e3b3d
S-codes14 <smlmnguni14@gmail.com>
2022-06-24 21:37:24 +0200

    save progress

---
+++ test.py
@@ -0,0 +1,24 @@
+import pymannkendall as mk
+import matplotlib.pyplot as plt
+ 
+gfg_data = [54, 52, 53, 59, 56, 57, 51, 52, 50, 53]
+ 
+
+"""
+    trend: This tells the trend-increasing, decreasing, or no trend.
+    h: True if the trend is present. False if no trend is present.
+    p: The p-value of the test.
+    z: The normalized test statistic.
+    Tau: Kendall Tau.
+    s: Mann-Kendal’s score
+    var_s: Variance S
+    slope: Theil-Sen estimator/slope
+    intercept: Intercept of Kendall-Theil Robust Line
+"""
+
+# perform Mann-Kendall Trend Test
+t = mk.original_test(gfg_data)
+print(t)
+
+plt.plot(gfg_data)
+plt.show()
